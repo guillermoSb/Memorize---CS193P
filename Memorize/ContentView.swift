1cd63679888eba0ecb83da17c528e873d63e62b8
guillermoSb <guillermo.santosba@gmail.com>
2021-05-06 18:53:05 -0600

    Lecture 1

--- Memorize/ContentView.swift
+++ Memorize/ContentView.swift
@@ -8,11 +8,85 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        Text("Hello, world!")
-            .padding()
+  // Define el tipo con some - Significa que el tipo de esa variable es cualquier cosa siempre que sea un view.
+  // Esta variable no es guardada si no que se calcula cada vez que se llama la variable
+  var body: some View {
+    // Esta en puntos
+    HStack {
+      ForEach(0..<4) { index in
+        CardView(isFaceUp: false)
+      }
     }
+    .padding()
+    .foregroundColor(.orange)
+    .font(.largeTitle)
+  }
 }
+
+
+struct CardView: View {
+  var isFaceUp: Bool
+  var body: some View {
+    ZStack {
+      if isFaceUp {
+        RoundedRectangle(cornerRadius: 10.0).fill(Color.white)
+        RoundedRectangle(cornerRadius: 10.0).stroke()
+        Text("👻")
+      } else {
+        // Fills using the foreground color on the Hstack
+        RoundedRectangle(cornerRadius: 10.0).fill()
+      }
+
+
+    }
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
